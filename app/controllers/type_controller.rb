ba34b20bc8165b996b65129e962dcc345acce00c
Hans Engel <engel@engel.uk.to>
2011-02-27 13:17:50 -0700

    force conversion of Fixnum -> String

--- app/controllers/type_controller.rb
+++ app/controllers/type_controller.rb
@@ -81,7 +81,7 @@ class TypeController < ApplicationController
   
   private
   def get_players room_id
-    REDIS.hgetall('rooms:id:' + room_id).map do |player_id, player_data_str|
+    REDIS.hgetall('rooms:id:' + room_id.to_s).map do |player_id, player_data_str|
       player_data = player_data_str.split ':'
       {'name' => player_data[0], 'wpm' => player_data[1], 'cpm' => player_data[2], 'done' => player_data[3]}
     end
