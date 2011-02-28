32cd54ecdab81ad61a5ca94fdf98d8cac73ecc1c
Hans Engel <engel@engel.uk.to>
2011-02-28 12:43:01 -0800

    Added gravatar_url() to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def gravatar_url user = nil
+    user ||= current_user
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png"
+  end
 end
