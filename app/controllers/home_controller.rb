594a9af4ce58026fce695f375b3ff8944da6ba87
Hans Engel <engel@engel.uk.to>
2011-03-05 13:54:32 -0700

    removed debug code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,6 @@ class HomeController < ApplicationController
     top_profile_ids = Record.find :all, :group => 'profile_id', :order => 'num_records desc',
       :select => 'count(1) as num_records, profile_id, avg(wpm) as avg_wpm, avg(cpm) as avg_cpm'
     @top_profiles = []
-    p top_profile_ids
     
     top_profile_ids.each do |profile|
       @top_profiles << [Profile.find(profile.profile_id), profile.num_records, profile.avg_wpm.to_f, profile.avg_cpm.to_f]
