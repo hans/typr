e0e587875b89859edebbec07d2378f813ed4d053
Hans Engel <engel@engel.uk.to>
2011-03-09 17:33:30 -0700

    don't let the user set empty emails / usernames. closes #34

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,8 +2,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def update
     case params[:form_type]
     when 'edit_user'
-      current_user.email = params[:user][:email]
-      current_user.username = params[:user][:username]
+      current_user.email = params[:user][:email] unless params[:user][:email].blank?
+      current_user.username = params[:user][:username] unless params[:user][:username].blank?
       current_user.save
       
       render :action => 'edit', :resource => current_user
