43c46be3e00299ea137a6b49a6edb350b7816529
Hans Engel <engel@engel.uk.to>
2011-03-13 13:12:52 -0700

    CoffeeScript rewrite all finished :) closes #36

--- public/javascripts/type.js
+++ public/javascripts/type.js
@@ -2,116 +2,114 @@
   var Compete, Practice, Type;
   var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
   Compete = (function() {
-    var COUNTDOWN_TIME, add_key_listener, add_player_row, countdown_el, find_room, first_poll, get_player_progress, highlight_leader, init_pollers, poll_server, prepare, room, server_poll, server_started, show_countdown, show_player_update, start, update_player_stats;
     function Compete(parent) {
       this.parent = parent;
+      this.poll_server = __bind(this.poll_server, this);;
+      this.countdown_time = 15;
+      this.countdown_el = null;
+      this.room = null;
+      this.server_poll = null;
+      this.first_poll = true;
+      this.server_started = false;
     }
-    COUNTDOWN_TIME = 15;
-    countdown_el = null;
-    room = null;
-    server_poll = null;
-    first_poll = true;
-    server_started = false;
-    prepare = function() {
-      countdown_el = $('#countdown');
-      return find_room();
+    Compete.prototype.prepare = function() {
+      this.countdown_el = $('#countdown');
+      return this.find_room();
     };
-    add_key_listener = function() {
-      return type_area.keyup(function(event) {
-        var can_show_error;
-        can_show_error = true;
-        return check_queue.push(event.keyCode);
-      });
+    Compete.prototype.add_key_listener = function() {
+      return this.parent.type_area.keyup(__bind(function(event) {
+        this.parent.can_show_error = true;
+        return this.parent.check_queue.push(event.keyCode);
+      }, this));
     };
-    find_room = function() {
-      return $.get('/type/compete/room/find', null, function(_room) {
-        room = _room;
-        room[0] = parseInt(room[0]);
-        $.each(room[1], function(idx, player) {
-          add_player_row(idx, player);
-          return show_player_update(idx, player);
-        });
-        this.parent.prepare_copy(room[2][0].split(' '), room[2][1]);
-        return init_pollers();
-      }, 'json');
+    Compete.prototype.find_room = function() {
+      return $.get('/type/compete/room/find', null, __bind(function(_room) {
+        this.room = _room;
+        this.room[0] = parseInt(this.room[0]);
+        $.each(this.room[1], __bind(function(idx, player) {
+          this.add_player_row(idx, player);
+          return this.show_player_update(idx, player);
+        }, this));
+        this.parent.prepare_copy(this.room[2][0].split(' '), this.room[2][1]);
+        return this.init_pollers();
+      }, this), 'json');
     };
-    start = function() {
-      return $.get("/type/compete/room/" + room[0] + "/start", null, function(data) {
-        var start_time, stats_poll;
+    Compete.prototype.start = function() {
+      return $.get("/type/compete/room/" + this.room[0] + "/start", null, __bind(function(data) {
         this.parent.hide_notifications();
-        start_time = new Date();
-        stats_poll = setInterval(calculate_stats, 500);
-        type_area.removeAttr('disabled').focus();
-        return add_key_listener();
-      });
+        this.parent.start_time = new Date();
+        this.parent.stats_poll = setInterval(this.parent.calculate_stats, 500);
+        this.parent.type_area.removeAttr('disabled').focus();
+        return this.add_key_listener();
+      }, this));
     };
-    update_player_stats = function(players) {
-      return $.each(players, function(idx, player) {
-        if (room[1][idx] != null) {
-          room[1][idx].wpm = player.wpm;
-          room[1][idx].cpm = player.cpm;
-          room[1][idx].cur_word_idx = player.cur_word_idx;
-          room[1][idx].done = player.done;
+    Compete.prototype.update_player_stats = function(players) {
+      return $.each(players, __bind(function(idx, player) {
+        if (this.room[1][idx] != null) {
+          this.room[1][idx].wpm = player.wpm;
+          this.room[1][idx].cpm = player.cpm;
+          this.room[1][idx].cur_word_idx = player.cur_word_idx;
+          this.room[1][idx].done = player.done;
         } else {
-          room[1][idx] = player;
-          add_player_row(idx, player);
+          this.room[1][idx] = player;
+          this.add_player_row(idx, player);
         }
-        return show_player_update(idx, player);
-      });
+        return this.show_player_update(idx, player);
+      }, this));
     };
-    add_player_row = function(cur_player_id, player) {
+    Compete.prototype.add_player_row = function(cur_player_id, player) {
       $('table.stats').append("<tr id='player-" + cur_player_id + "'><td>" + player.name + "</td><td></td><td>" + player.wpm + "</td><td>" + player.cpm + "</td></tr>");
       return $($("table.stats tr#player-" + cur_player_id + " td")[1]).progressbar();
     };
-    show_player_update = function(cur_player_id, player) {
+    Compete.prototype.show_player_update = function(cur_player_id, player) {
       var columns;
       columns = $("table.stats tr#player-" + cur_player_id + " td");
-      $(columns[1]).progressbar('value', get_player_progress(player.cur_word_idx) * 100);
+      $(columns[1]).progressbar('value', this.get_player_progress(player.cur_word_idx) * 100);
       $(columns[2]).text(player.wpm);
       return $(columns[3]).text(player.cpm);
     };
-    get_player_progress = function(player_cur_word_idx) {
-      return player_cur_word_idx / num_words;
+    Compete.prototype.get_player_progress = function(player_cur_word_idx) {
+      return player_cur_word_idx / this.parent.num_words;
     };
-    show_countdown = function(time_left) {
-      return countdown_el.text(time_left);
+    Compete.prototype.show_countdown = function(time_left) {
+      return this.countdown_el.text(time_left);
     };
-    highlight_leader = function() {
+    Compete.prototype.highlight_leader = function() {
       var sorted_players;
-      sorted_players = $.keys(room[1]);
+      sorted_players = $.keys(this.room[1]);
       sorted_players = sorted_players.sort(function(a, b) {
-        return room[1][b].cur_word_idx - room[1][a].cur_word_idx;
+        return this.room[1][b].cur_word_idx - this.room[1][a].cur_word_idx;
       });
       if (sorted_players[0].id === player_id && (sorted_players[1] != null)) {
         this.parent.words.removeClass('leader-highlight');
-        return $(this.parent.words[room[1][sorted_players[1]].cur_word_idx]).addClass('leader-highlight');
+        return $(this.parent.words[this.room[1][sorted_players[1]].cur_word_idx]).addClass('leader-highlight');
       } else {
         this.parent.words.removeClass('leader-highlight');
-        return $(this.parent.words[room[1][sorted_players[0]].cur_word_idx]).addClass('leader-highlight');
+        return $(this.parent.words[this.room[1][sorted_players[0]].cur_word_idx]).addClass('leader-highlight');
       }
     };
-    init_pollers = function() {
-      server_poll = setInterval(poll_server, 2000);
-      return this.parent.poll = setInterval(eval_next_in_queue, 30);
+    Compete.prototype.init_pollers = function() {
+      this.server_poll = setInterval(this.poll_server, 2000);
+      return this.parent.poll = setInterval(this.parent.eval_next_in_queue, 30);
     };
-    poll_server = function() {
+    Compete.prototype.poll_server = function() {
       var time_before_start, url;
-      if (typeof start_time == "undefined" || start_time === null) {
-        time_before_start = room[0] + COUNTDOWN_TIME - (new Date().getTime() / 1000);
-        show_countdown(Math.round(time_before_start));
-        if (!server_started && time_before_start < 0) {
-          server_started = true;
-          start();
+      if (this.parent.start_time == null) {
+        time_before_start = this.room[0] + this.countdown_time - (new Date().getTime() / 1000);
+        this.show_countdown(Math.round(time_before_start));
+        if (!this.server_started && time_before_start < 0) {
+          this.server_started = true;
+          this.start();
         } else {
-          setTimeout(poll_server, 1000);
+          setTimeout(this.poll_server, 1000);
         }
         return;
       }
-      if (first_poll) {
-        first_poll = false;
+      if (this.first_poll) {
+        this.first_poll = false;
         return;
       }
-      url = "/type/compete/room/" + room[0] + "/" + player_id;
+      url = "/type/compete/room/" + this.room[0] + "/" + player_id;
       return $.post(url, {
         player_id: player_id,
         player_name: player_name,
@@ -119,10 +117,10 @@
         cpm: this.parent.stat_cpm,
         cur_word_idx: this.parent.cur_word_idx,
         done: this.parent.is_done
-      }, function(data) {
-        update_player_stats(data[1]);
-        return highlight_leader();
-      }, 'json');
+      }, __bind(function(data) {
+        this.update_player_stats(data[1]);
+        return this.highlight_leader();
+      }, this), 'json');
     };
     return Compete;
   })();
--- public/javascripts/type/compete.coffee
+++ public/javascripts/type/compete.coffee
@@ -1,129 +1,128 @@
 class Compete
 	constructor: (@parent) ->
-	
-	# length of the countdown, in seconds
-	COUNTDOWN_TIME = 15
-	countdown_el = null
+		# length of the countdown, in seconds
+		@countdown_time = 15
+		@countdown_el = null
 
-	# room data container
-	room = null
+		# room data container
+		@room = null
 
-	# polls server for player / room status
-	server_poll = null
-	first_poll = true
+		# polls server for player / room status
+		@server_poll = null
+		@first_poll = true
 
-	server_started = false
+		@server_started = false
 
-	prepare = ->
-		countdown_el = $('#countdown')
-		find_room()
+	prepare: ->
+		@countdown_el = $('#countdown')
+		this.find_room()
 
-	add_key_listener = ->
-		type_area.keyup (event) ->
-			can_show_error = true
-			check_queue.push event.keyCode
+	add_key_listener: ->
+		@parent.type_area.keyup (event) =>
+			@parent.can_show_error = true
+			@parent.check_queue.push event.keyCode
 
-	find_room = ->
-		$.get '/type/compete/room/find', null, (_room) ->
+	find_room: ->
+		$.get '/type/compete/room/find', null, (_room) =>
 			# set the global room object, and make sure its ID is an int
 			# (for use in the countdown)
-			room = _room
-			room[0] = parseInt room[0]
+			@room = _room
+			@room[0] = parseInt @room[0]
 		
-			$.each room[1], (idx, player) ->
-				add_player_row idx, player
-				show_player_update idx, player
+			$.each @room[1], (idx, player) =>
+				this.add_player_row idx, player
+				this.show_player_update idx, player
 		
-			this.parent.prepare_copy room[2][0].split(' '), room[2][1]
-			init_pollers()
+			@parent.prepare_copy @room[2][0].split(' '), @room[2][1]
+			this.init_pollers()
 		, 'json'
 
-	start = ->
-		$.get "/type/compete/room/#{room[0]}/start", null, (data) ->
-			this.parent.hide_notifications()
-			start_time = new Date()
-			stats_poll = setInterval calculate_stats, 500
+	start: ->
+		$.get "/type/compete/room/#{@room[0]}/start", null, (data) =>
+			@parent.hide_notifications()
+			@parent.start_time = new Date()
+			@parent.stats_poll = setInterval @parent.calculate_stats, 500
 		
-			type_area.removeAttr('disabled').focus()
-			add_key_listener()
+			@parent.type_area.removeAttr('disabled').focus()
+			this.add_key_listener()
 
 	# process stats received from the server
-	update_player_stats = (players) ->
-		$.each players, (idx, player) ->
-			if room[1][idx]?
-				room[1][idx].wpm = player.wpm
-				room[1][idx].cpm = player.cpm
-				room[1][idx].cur_word_idx = player.cur_word_idx
-				room[1][idx].done = player.done
+	update_player_stats: (players) ->
+		$.each players, (idx, player) =>
+			if @room[1][idx]?
+				@room[1][idx].wpm = player.wpm
+				@room[1][idx].cpm = player.cpm
+				@room[1][idx].cur_word_idx = player.cur_word_idx
+				@room[1][idx].done = player.done
 			else
-				room[1][idx] = player
-				add_player_row idx, player
+				@room[1][idx] = player
+				this.add_player_row idx, player
 		
-			show_player_update idx, player
+			this.show_player_update idx, player
 
 	# add a row into the stats table for a given player
-	add_player_row = (cur_player_id, player) ->
+	add_player_row: (cur_player_id, player) ->
 		$('table.stats').append "<tr id='player-#{cur_player_id}'><td>#{player.name}</td><td></td><td>#{player.wpm}</td><td>#{player.cpm}</td></tr>"
 		$($("table.stats tr#player-#{cur_player_id} td")[1]).progressbar()
 
 	# update a certain player's progress bar and stats
-	show_player_update = (cur_player_id, player) ->
+	show_player_update: (cur_player_id, player) ->
 		columns = $("table.stats tr#player-#{cur_player_id} td")
-		$(columns[1]).progressbar 'value', get_player_progress(player.cur_word_idx) * 100
+		$(columns[1]).progressbar 'value', this.get_player_progress(player.cur_word_idx) * 100
 		$(columns[2]).text player.wpm
 		$(columns[3]).text player.cpm
 
-	get_player_progress = (player_cur_word_idx) ->
-		player_cur_word_idx / num_words
+	get_player_progress: (player_cur_word_idx) ->
+		player_cur_word_idx / @parent.num_words
 
-	show_countdown = (time_left) ->
-		countdown_el.text time_left
+	show_countdown: (time_left) ->
+		@countdown_el.text time_left
 
-	highlight_leader = ->
-		sorted_players = $.keys room[1]
+	highlight_leader: ->
+		sorted_players = $.keys @room[1]
 		sorted_players = sorted_players.sort (a, b) ->
-			room[1][b].cur_word_idx - room[1][a].cur_word_idx
+			@room[1][b].cur_word_idx - @room[1][a].cur_word_idx
 	
 		# highlight the leader in red.
 		# if the current user is the leader, highlight the user in second place
 		if sorted_players[0].id is player_id and sorted_players[1]?
-			this.parent.words.removeClass 'leader-highlight'
-			$(this.parent.words[room[1][sorted_players[1]].cur_word_idx]).addClass 'leader-highlight'
+			@parent.words.removeClass 'leader-highlight'
+			$(@parent.words[@room[1][sorted_players[1]].cur_word_idx]).addClass 'leader-highlight'
 		else
-			this.parent.words.removeClass 'leader-highlight'
-			$(this.parent.words[room[1][sorted_players[0]].cur_word_idx]).addClass 'leader-highlight'
+			@parent.words.removeClass 'leader-highlight'
+			$(@parent.words[@room[1][sorted_players[0]].cur_word_idx]).addClass 'leader-highlight'
 
-	init_pollers = ->
-		server_poll = setInterval poll_server, 2000
-		this.parent.poll = setInterval eval_next_in_queue, 30
+	init_pollers: ->
+		@server_poll = setInterval this.poll_server, 2000
+		@parent.poll = setInterval @parent.eval_next_in_queue, 30
 
-	poll_server = ->
-		unless start_time?
-			time_before_start = room[0] + COUNTDOWN_TIME - ( new Date().getTime() / 1000 )
-			show_countdown Math.round time_before_start
+	poll_server: =>
+		unless @parent.start_time?
+			time_before_start = @room[0] + @countdown_time - ( new Date().getTime() / 1000 )
+			this.show_countdown Math.round time_before_start
 		
-			if !server_started and time_before_start < 0
-				server_started = true
-				start()
+			if !@server_started and time_before_start < 0
+				@server_started = true
+				this.start()
 			else
 				# hack - get this code to re-run every one second until things get started
-				setTimeout poll_server, 1000
+				setTimeout this.poll_server, 1000
 		
 			return
 	
-		if first_poll
-			first_poll = false
+		if @first_poll
+			@first_poll = false
 			return
 	
-		url = "/type/compete/room/#{room[0]}/#{player_id}"
+		url = "/type/compete/room/#{@room[0]}/#{player_id}"
 		$.post url,
 			player_id: player_id
 			player_name: player_name
-			wpm: this.parent.stat_wpm
-			cpm: this.parent.stat_cpm
-			cur_word_idx: this.parent.cur_word_idx
-			done: this.parent.is_done
-		, (data) ->
-			update_player_stats data[1]
-			highlight_leader()
+			wpm: @parent.stat_wpm
+			cpm: @parent.stat_cpm
+			cur_word_idx: @parent.cur_word_idx
+			done: @parent.is_done
+		, (data) =>
+			this.update_player_stats data[1]
+			this.highlight_leader()
 		, 'json'
